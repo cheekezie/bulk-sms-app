e9d3970ca10ae9858c2a814c0141f09700755c87
Chikezie Harrison <47220057+cheekezie@users.noreply.github.com>
2025-08-26 01:05:35 +0100

    envs updated

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,12 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   envName: 'DEV',
-  baseUrl: 'https://dev.api.fees.ng/api/v2/',
-  examportalUrl: 'https://staging.api.examportal.ng/api/v1/school/profile/',
-  examportalKey: 'sllwjKQ9UiUb',
-  idleTimeAllowed: 600,
-  idleTimeout: 5,
-  squadGateway: true,
-  isSandbox: true,
-  monifyUrl: 'https://sandbox.sdk.monnify.com/checkout/',
+  baseUrl: 'http://localhost:3200/api/v1/',
 };
